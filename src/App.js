97b2ad60be43e336cc2c33bb371348acfadbb12d
Davimuler <davidbarba608@gmail.com>
2024-11-04 11:54:18 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="header">
+                ShopName
+            </header>
+            <div className="main">
+                <aside className="sidebar">
+                    Sidebar
+                </aside>
+                <main className="content">
+                   Main content
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default App;
